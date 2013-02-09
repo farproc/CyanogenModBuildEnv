22ac79de97906c56d87fc567f9f36b6aa756f158
Tim Bateman <tim.bateman@gmail.com>
2013-02-09 14:21:34 -0500

    Added basic vagrant and puppet configs

---
+++ Vagrantfile
@@ -0,0 +1,8 @@
+Vagrant::Config.run do |config|
+
+  config.vm.define :precise64 do |precise64_config|
+    precise64_config.vm.box = "precise64"
+    precise64_config.vm.provision :puppet
+  end
+
+end
---
+++ manifests/default.pp
@@ -0,0 +1,75 @@
+
+class prep-android-build {
+    Exec {
+        path => [
+            '/usr/local/bin',
+            '/opt/local/bin',
+            '/usr/bin', 
+            '/usr/sbin', 
+            '/bin',
+            '/sbin',
+            '/root/bin' ],
+        logoutput => false,
+    }
+
+    exec { "apt-get update":
+	command => "/usr/bin/apt-get update",
+    }
+
+    Package {
+        ensure => present,
+        require => Exec['apt-get update'],
+    }
+
+    package { "build-essential": }
+    package { "git-core": }
+    package { "curl": }
+    package { "gnupg": }
+    package { "flex": }
+    package { "bison": }
+    package { "gperf": }
+    package { "libsdl1.2-dev": }
+    package { "libesd0-dev": }
+    package { "libwxgtk2.8-dev": }
+    package { "squashfs-tools": }
+    package { "zip": }
+    package { "libncurses5-dev": }
+    package { "zlib1g-dev": }
+    package { "openjdk-6-jre": }
+    package { "openjdk-6-jdk": }
+    package { "pngcrush": }
+    package { "schedtool": }
+    package { "libxml2": }
+    package { "xsltproc": }
+    package { "g++-multilib": }
+    package { "lib32z1-dev": }
+    package { "lib32ncurses5-dev": }
+    package { "lib32readline-gplv2-dev": }
+    package { "gcc-4.6-multilib": } # would ideally like 4.7
+    package { "g++-4.5-multilib": }
+    package { "libxml2-utils": }
+
+    exec { 'install repo':
+        command => 'mkdir -p /root/bin && curl https://dl-ssl.google.com/dl/googlesource/git-repo/repo > /root/bin/repo && chmod u+x /root/bin/repo',
+        creates => '/root/bin/repo',
+        require => [ Package['curl'], Package['git-core'] ],
+    }
+
+    file { "/root/.gitconfig" :
+        source => "/vagrant/gitconfig",
+        owner => 'root',
+        group => 'root',
+    }
+
+    # repo init/sync:
+    # repo init -u git://github.com/CyanogenMod/android.git -b cm-10.1
+    # repo sync
+
+    # get prebuilt
+    # cd ~/android/system/vendor/cm
+    # ./get-prebuilts
+
+}
+
+include prep-android-build
+
